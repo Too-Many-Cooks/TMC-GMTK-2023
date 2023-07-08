c5151416c55107f5b7fb282ffe6c64b3f892bd1a
Kaethon <johanneskloeckner@web.de>
2023-07-08 02:45:04 +0200

    Add basic hero beat actions (stuff that the hero does); Add action sequencer (manager that plays sequences of actions); Create beat manager prefab;

---
+++ Assets/Scripts/HeroActionSequencer/HeroActionSequencer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using static BeatManager;
+
+public class HeroActionSequencer : MonoBehaviour
+{
+    [SerializeField]
+    ActionSequence heroActionSequencer;
+    int currentActionSequencerIndex = 0;
+
+    [SerializeField]
+    UnityEvent<HeroBeatAction> HeroBeatActionEvent;
+
+    public void OnBeat(BeatEventData beatEventData)
+    {
+        HeroBeatActionEvent.Invoke(heroActionSequencer.heroBeatActionSequence[currentActionSequencerIndex]);
+        currentActionSequencerIndex += 1;
+        currentActionSequencerIndex %= heroActionSequencer.heroBeatActionSequence.Count;
+    }
+}

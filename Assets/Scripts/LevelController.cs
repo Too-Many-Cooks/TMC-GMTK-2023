d4dddc8d3471e429415a5f3a8836148648f91f06
Daniel Kelly <dlpkelly@hotmail.com>
2023-07-08 05:37:47 -0400

    Initial level shifting

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private float shiftDistance = 3f;
+    [SerializeField] private float shiftDuration = 0.15f;
+    [SerializeField] private float shiftCooldown = 0.5f;
+    [SerializeField] private float shiftQueueTime = 0.15f;
+
+    private float ShiftSpeed {  get { return shiftDistance / (TrueShiftDuration); } }
+    private float TrueShiftDuration { get { return ShiftTicks * Time.fixedDeltaTime; } }
+    private int ShiftTicks { get { return Mathf.FloorToInt(shiftDuration / Time.fixedDeltaTime); } }
+    private float TrueShiftCooldown { get { return CooldownTicks * Time.fixedDeltaTime; } }
+    private int CooldownTicks { get { return Mathf.FloorToInt(shiftCooldown / Time.fixedDeltaTime); } }
+
+    private float shiftTimer = 0f;
+    private float shiftCooldownTimer = 0f;
+    private Vector2 queuedShift = Vector2.zero;
+    private Vector2 currentShift = Vector2.zero;
+    private Vector2 shiftEnd = Vector2.zero;
+
+    private new Rigidbody2D rigidbody2D;
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(shiftCooldownTimer > 0f)
+        {
+            shiftCooldownTimer -= Time.fixedDeltaTime;
+        }
+        if (shiftCooldownTimer < 0f)
+        {
+            shiftCooldownTimer = 0f;
+        }
+
+        if(shiftTimer > 0f)
+        {
+            shiftTimer -= Time.deltaTime;
+        }
+        if (shiftTimer < 0f)
+        {
+            shiftTimer = 0f;
+        }
+
+        //Handle current shift
+        if (shiftTimer <= 0f && shiftCooldownTimer <= 0f && queuedShift != Vector2.zero)
+        {
+            currentShift = queuedShift;
+            queuedShift = Vector2.zero;
+            shiftCooldownTimer = TrueShiftCooldown;
+            shiftTimer = TrueShiftDuration;
+            shiftEnd = rigidbody2D.position + currentShift * shiftDistance;
+        }
+
+        if (shiftTimer > 0f )
+        {
+            var velocity = rigidbody2D.velocity;
+            var frameShift = Vector2.SmoothDamp(rigidbody2D.position, shiftEnd, ref velocity, shiftTimer, Mathf.Infinity, Time.fixedDeltaTime);
+            rigidbody2D.velocity = velocity;
+        }
+
+        if(shiftTimer < 0f)
+        {
+            currentShift = Vector2.zero;
+        }
+    }
+
+    public void OnShiftUp()
+    {
+        if(shiftCooldownTimer < shiftQueueTime)
+        {
+            queuedShift = Vector2.up;
+        }
+    }
+
+    public void OnShiftDown()
+    {
+        if (shiftCooldownTimer < shiftQueueTime)
+        {
+            queuedShift = Vector2.down;
+        }
+    }
+
+    public void OnShiftLeft()
+    {
+        if (shiftCooldownTimer < shiftQueueTime)
+        {
+            queuedShift = Vector2.left;
+        }
+    }
+
+    public void OnShiftRight()
+    {
+        if (shiftCooldownTimer < shiftQueueTime)
+        {
+            queuedShift = Vector2.right;
+        }
+    }
+}

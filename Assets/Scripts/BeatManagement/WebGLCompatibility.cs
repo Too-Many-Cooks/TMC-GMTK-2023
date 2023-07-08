013c4244243189cd25c7948813a8b4e95a161ca1
Daniel Kelly <dlpkelly@hotmail.com>
2023-07-08 03:02:27 -0400

    Added audio autoplay and editor buttons for WebGLCompatability script

--- Assets/Scripts/BeatManagement/WebGLCompatibility.cs
+++ Assets/Scripts/BeatManagement/WebGLCompatibility.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class WebGLCompatibility : MonoBehaviour
 {
+    public bool autoplay = false;
+
     AudioSource audioSource;
     bool isPaused = false;
     float pauseTime;
@@ -13,6 +16,14 @@ public class WebGLCompatibility : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        if (autoplay)
+        {
+            Play();
+        }
+    }
+
+    private void Update()
+    {
     }
 
     public void Play()
@@ -33,6 +44,28 @@ public class WebGLCompatibility : MonoBehaviour
         {
             audioSource.time = pauseTime;
             audioSource.UnPause();
+        }
+    }
+}
+
+[CustomEditor(typeof(WebGLCompatibility))]
+public class WebGLCompatibilityEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        WebGLCompatibility compatabilityLayer = (WebGLCompatibility)target;
+        if (GUILayout.Button("Play"))
+        {
+            compatabilityLayer.Play();
+        }
+        if (GUILayout.Button("Pause"))
+        {
+            compatabilityLayer.Pause();
+        }
+        if (GUILayout.Button("Unpause"))
+        {
+            compatabilityLayer.UnPause();
         }
     }
 }
